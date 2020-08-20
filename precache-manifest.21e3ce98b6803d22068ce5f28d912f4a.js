49ba2e9e2e4fd9c2f28e140e3fae289432f842d3
Meet Patel <patelmeet59@gmail.com>
2020-08-20 21:54:53 +0530

    Updates

---
+++ precache-manifest.21e3ce98b6803d22068ce5f28d912f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b81de56f1245f1c1e8d50bb3d60c6d2",
+    "url": "/dsc-slop-website/index.html"
+  },
+  {
+    "revision": "d24647df820e80f9f4ad",
+    "url": "/dsc-slop-website/static/js/2.0ef6a746.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/dsc-slop-website/static/js/2.0ef6a746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c206ba8188df2df2fd3",
+    "url": "/dsc-slop-website/static/js/main.b8ab5ffc.chunk.js"
+  },
+  {
+    "revision": "406431086c6f2462d7a4",
+    "url": "/dsc-slop-website/static/js/runtime-main.d20f52ad.js"
+  },
+  {
+    "revision": "1bf3ab4ef257c20652f639ee9e168554",
+    "url": "/dsc-slop-website/static/media/bg.1bf3ab4e.jpg"
+  }
+]);
