6476681439a93911bc8ed85cdcf0fc3912934130
Hemant Jain <hmtj99@gmail.com>
2020-08-22 18:28:49 +0530

    Added Search Function To Leaderboard Table

--- src/Pages/LeaderBoard.js
+++ src/Pages/LeaderBoard.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import LeaderBoardTable from '../components/LeaderBoardTable';
+import LeaderBoardTable2 from '../components/LeaderBoardTable2';
 
 const LeaderBoard = () => {
-  const iframe =
-    '<iframe class="airtable-embed" src = "https://airtable.com/embed/shroD5TJj8KuroIXS?backgroundColor=blue&viewControls=on" frameborder = "0" onmousewheel = "" width = "100%" height = "533" style = "background: transparent; border: 1px solid #ccc;" ></iframe >';
+  //const iframe =
+  // '<iframe class="airtable-embed" src = "https://airtable.com/embed/shroD5TJj8KuroIXS?backgroundColor=blue&viewControls=on" frameborder = "0" onmousewheel = "" width = "100%" height = "533" style = "background: transparent; border: 1px solid #ccc;" ></iframe >';
   return (
     // <>
     //   <div dangerouslySetInnerHTML={{ __html: iframe ? iframe : '' }} />
     // </>
-    <LeaderBoardTable />
+    <>
+      <LeaderBoardTable2 />
+    </>
   );
 };
 
---
+++ src/components/LeaderBoardTable2.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import Grid from '@material-ui/core/Grid';
+import Grow from '@material-ui/core/Grow';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import axios from 'axios';
+
+function LeaderBoardTable2() {
+    const [users, setUsers] = useState([]);
+    const [loaded, setLoaded] = React.useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("https://script.google.com/macros/s/AKfycbxAfEhHRFKiwJ05oG1Zw4vF5sXqdnXKm2d6NP3QrF7C3oIhbxY/exec");
+            let usersList = response.data.user;
+            setUsers(usersList);
+            setLoaded(true);
+        }
+        fetchData();
+    }, [])
+
+    if (loaded === false) {
+        return (
+            <Grid container spacing={3} justify="center" alignItems="center" style={{ height: '100vh', textAlign: "center" }}>
+                <Grid item>
+                    <CircularProgress size={100} />
+                </Grid>
+            </Grid>
+        );
+    } else {
+        return (
+            <Grow in={true}>
+                <MaterialTable
+                    title="Leaderboard"
+                    columns={[
+                        { title: '       Current Rank', field: 'rank', align: "center", headerStyle: { whiteSpace: 'pre' } },
+                        { title: '       Username', field: 'username', align: "center", headerStyle: { whiteSpace: 'pre' } },
+                        { title: '       Points', field: 'points', align: "center", headerStyle: { whiteSpace: 'pre' } },
+                        { title: '       Pull Requests', field: 'pull_request', align: "center", headerStyle: { whiteSpace: 'pre' } },
+                    ]}
+                    data={users}
+                    isLoading={!loaded}
+                    options={{
+                        rowStyle: x => {
+                            if (x.tableData.id % 2 === 0) {
+                                return { backgroundColor: '#434543' }
+                            }
+                        },
+                        pageSize: 6,
+                        pageSizeOptions: [6, 12, 30]
+                    }}
+                />
+            </Grow>
+        )
+    }
+
+}
+
+export default LeaderBoardTable2;
