309f60a1957cea6e90bbf43ed3bd2b44f3415696
Smit Shah <smir2k14@gmail.com>
2020-08-24 23:39:58 +0530

    chore: refactor the contacts page

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import Contact from '../components/Contact';
-import { Container, Grid, Grow,Typography, Divider } from '@material-ui/core';
-import organizers from '../components/organizers';
+import React from "react";
+import Contact from "../components/Contact";
+import { Container, Grid, Grow, Typography, Divider } from "@material-ui/core";
+import organizers from "../components/organizers";
 
 const Contacts = () => {
   return (
     <>
       <Container className="my-3">
-        <Typography className="text-center" variant="h4" color="textPrimary"><b>Organizers</b></Typography>
-        <Divider className="my-4" variant="middle"/>
-          <Grow in>
-              <Grid container>
-                  {
-                      organizers.map((member,id)=>{
-                            return(
-                                <Grid key={id} item xs={12} sm={6} md={4}>
-                                    <Contact details={member}/>
-                                </Grid>
-                            )                          
-                      })
-                  }
-              </Grid>
-          </Grow>
+        <Typography className="text-center" variant="h4" color="textPrimary">
+          <b>Meet the Team!</b>
+        </Typography>
+        <Typography className="text-center" variant="h6" color="textPrimary">
+          <p>
+            We are a group of development enthusiasts who want to increase the
+            development and open-source culture among students.
+          </p>
+        </Typography>
+        <Divider className="my-4" variant="middle" />
+        <Grow in>
+          <Grid container>
+            {organizers.map((member, id) => {
+              return (
+                <Grid key={id} item xs={12} sm={8} md={6}>
+                  <Contact details={member} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Grow>
       </Container>
     </>
   );
