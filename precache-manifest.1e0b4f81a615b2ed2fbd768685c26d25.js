4cde210f9b599e4bc1286acc51cd74c726252278
Vrutik <vrutikprajapati2809@gmail.com>
2022-08-31 21:48:39 +0530

    add .gitignore fix bug

---
+++ precache-manifest.1e0b4f81a615b2ed2fbd768685c26d25.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbac0fa40a41f4e65b3823f10fa9d70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecc4d765d23eedeb973a",
+    "url": "/static/css/main.4ec41802.chunk.css"
+  },
+  {
+    "revision": "b433b54f1850d6280683",
+    "url": "/static/js/2.4d95e71b.chunk.js"
+  },
+  {
+    "revision": "be0a61cdb69913bf747c0a3dd7b0357d",
+    "url": "/static/js/2.4d95e71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc4d765d23eedeb973a",
+    "url": "/static/js/main.d9292581.chunk.js"
+  },
+  {
+    "revision": "ffe406a6f751f6376c05",
+    "url": "/static/js/runtime-main.646d1c6c.js"
+  },
+  {
+    "revision": "e2e07898f75177cca098f1d6e9c7041d",
+    "url": "/static/media/bg.e2e07898.png"
+  },
+  {
+    "revision": "94a393734c4fb2055a911445a9e3ff88",
+    "url": "/static/media/linuxtux.94a39373.svg"
+  },
+  {
+    "revision": "efdd8a78ba9441e2c266c04571cbdff4",
+    "url": "/static/media/logo.efdd8a78.png"
+  },
+  {
+    "revision": "777a978fc77f5b57c9d0cbd1c2fc7ad9",
+    "url": "/static/media/man.777a978f.png"
+  }
+]);
