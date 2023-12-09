af66d03f511a8926b95395de3b2c1d462c161c88
Shivam M <94903454+ShivamMadlani@users.noreply.github.com>
2023-12-09 23:22:00 +0530

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -139,7 +139,7 @@ export default function HomePage() {
                       className="mt-8 xs:text-xl text-3xl blink blink-two"
                     >
                       <div className="txt--gradient-orange">
-                        <b>🚀Contribution Period Has Ended🚀</b>
+                        <b>Contribution Period Has Ended.</b>
                       </div>
                     </Typography>
                   </div>
