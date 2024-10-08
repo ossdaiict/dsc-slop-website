92f9aead78b51f2e8e8120ca1d99a98819f2fc87
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2024-10-08 15:20:00 +0530

    chore(dates/links): updated dates links etc.

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -14,7 +14,7 @@ const CountdownDisplay = (props) => {
     return (
         <>
             <Typography className="text-center txt--gradient-orange font-sans xs:text-2xl text-4xl">
-                🚀Mentor Registrations has Started🚀
+                🚀Student Registrations has Started🚀
             </Typography>
 
             {/* <div className="flex w-full justify-center mt-4">
